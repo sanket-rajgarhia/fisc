d00771bf6186d9fe96ff96f72462daaa0c0e9b51
sanket <sanket.rajgarhia@gmail.com>
2023-05-24 11:47:48 +0700

    Component1-L33 : Created a custom Expense Item Component HTML structure'

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <p>24 May 2023</p>
+            </div>
+            <div>
+                <p>Car Insurance</p>
+                <p>$234</p>
+            </div>
+        </div>);
+}
+
+export default ExpenseItem;
